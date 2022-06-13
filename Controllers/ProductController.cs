4d582b13412a22a472eb33b2efcced5ce0f87033
Carl Li <leekunhui@sina.cn>
2022-06-14 08:24:44 +1200

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Inventory.Entities;
+
+namespace Inventory.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<Product> GetAllProducts()
+        {
+            Product product = new Product();
+            product.Name = "Apple";
+            product.Stock = 5;
+            product.Unit = "Pack";
+
+            return product;
+        }
+    }
+}
---
+++ Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Inventory.Entities
+{
+    public class Product
+    {
+        // Product (Id, Name, Unit, IsWatching, WarnLevel, IsActive, Stock)
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string Unit { get; set; } = "";
+        public bool IsWatching { get; set; }
+        public int WarnLevel { get; set; }
+        public bool IsActive { get; set; }
+        public int Stock { get; set; } = 0;
+    }
+}
